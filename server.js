5ab4050cf1ca2814b09e6d90ca3e7d5581ea9147
rayadprohubofficiel-collab <rayadpro.hub.officiel@gmail.com>
2026-09-18 10:12:47 +0000

    Create server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/connect', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'connect.html'));
+});
+
+app.get('/dashboard', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'dashboard.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`RAYAD MD running on port ${PORT}`);
+});
